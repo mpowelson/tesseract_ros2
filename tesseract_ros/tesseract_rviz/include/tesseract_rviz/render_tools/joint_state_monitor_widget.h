f68fbac386c08b4480ee7b4117b34b8fa1f4cbf2
mpowelson <matthew.powelson@swri.org>
2020-02-24 14:54:53 -0600

    joint state monitor

--- tesseract_ros/tesseract_rviz/include/tesseract_rviz/render_tools/joint_state_monitor_widget.h
+++ tesseract_ros/tesseract_rviz/include/tesseract_rviz/render_tools/joint_state_monitor_widget.h
@@ -8,9 +8,8 @@ TESSERACT_COMMON_IGNORE_WARNINGS_POP
 
 #ifndef Q_MOC_RUN
 TESSERACT_COMMON_IGNORE_WARNINGS_PUSH
-#include <ros/ros.h>
-#include <ros/service_server.h>
-#include <sensor_msgs/JointState.h>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/joint_state.hpp>
 TESSERACT_COMMON_IGNORE_WARNINGS_POP
 #include <tesseract/tesseract.h>
 #endif
@@ -33,14 +32,14 @@ public:
   using Ptr = std::shared_ptr<JointStateMonitorWidget>;
   using ConstPtr = std::shared_ptr<const JointStateMonitorWidget>;
 
-  JointStateMonitorWidget(rviz_common::properties::Property* widget, rviz::Display* display);
+  JointStateMonitorWidget(rviz_common::properties::Property* widget, rviz_common::Display* display);
 
   virtual ~JointStateMonitorWidget();
 
   void onInitialize(VisualizationWidget::Ptr visualization,
                     tesseract::Tesseract::Ptr tesseract,
                     rviz_common::DisplayContext* context,
-                    ros::NodeHandle update_nh);
+                    rclcpp::Node::SharedPtr update_node);
 
   void onEnable();
   void onDisable();
@@ -52,19 +51,19 @@ private Q_SLOTS:
 
 protected:
   rviz_common::properties::Property* widget_;
-  rviz::Display* display_;
+  rviz_common::Display* display_;
   VisualizationWidget::Ptr visualization_;
   tesseract::Tesseract::Ptr tesseract_;
-  ros::NodeHandle nh_;
-  ros::Subscriber joint_state_subscriber_;
+  rclcpp::Node::SharedPtr node_;
+  rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr joint_state_subscriber_;
   bool update_required_;
 
   rviz_common::properties::Property* main_property_;
-  rviz::RosTopicProperty* joint_state_topic_property_;
+  rviz_common::properties::RosTopicProperty* joint_state_topic_property_;
 
-  void newJointStateCallback(const sensor_msgs::JointStateConstPtr& joint_state);
+  void newJointStateCallback(const sensor_msgs::msg::JointState::ConstSharedPtr joint_state);
 
-  bool isUpdateRequired(const sensor_msgs::JointState& joint_state);
+  bool isUpdateRequired(const sensor_msgs::msg::JointState& joint_state);
 };
 }  // namespace tesseract_rviz
 #endif  // TESSERACT_RVIZ_STATE_MONITORING_H
